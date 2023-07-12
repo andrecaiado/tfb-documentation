afcbb742e0162999e0f7518b20713fd4393a9227
André Caiado <andrecaiado@gmail.com>
2023-07-12 12:29:38 +0100

    ci: Add yarn.lock

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://tfb-documentation.github.io',
+  url: 'https://andrecaiado.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tfb-documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'andrecaiado', // Usually your GitHub org/user name.
-  projectName: 'tfb-documentation.github.io', // Usually your repo name.
+  projectName: 'tfb-documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
